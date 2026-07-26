bbc8cdd61a98705350dfc0dffe4f79936dba1b00
Versha-sharma16 <vershasharma162014@gmail.com>
2026-07-26 21:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as go
-
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 # -----------------------------
 # PAGE CONFIGURATION
 # -----------------------------
