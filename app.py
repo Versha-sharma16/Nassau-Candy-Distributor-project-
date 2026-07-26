6e7f49b37e100f4240667dab799573e55f35494e
Versha-sharma16 <vershasharma162014@gmail.com>
2026-07-26 21:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,8 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "e00e4eda-4bcd-4ee9-903e-73aaae8393ca",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Overwriting app.py\n"
-     ]
-    }
-   ],
-   "source": [
-    "%%writefile app.py\n",
-    "\n",
-    "import streamlit as st\n",
-    "import pandas as pd\n",
-    "\n",
-    "st.title(\"Nassau Candy Dashboard\")\n",
-    "\n",
-    "df = pd.read_csv(\"Nassau Candy Distributor.csv\")\n",
-    "\n",
-    "st.write(df.head())"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+import pandas as pd
+
+st.title("🍬 Nassau Candy Dashboard")
+
+df = pd.read_csv("Nassau Candy Distributor.csv")
+
+st.write(df.head())
